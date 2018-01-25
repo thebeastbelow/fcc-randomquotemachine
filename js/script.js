6ba74e7b45e0ffc837f031772359782cc63f8111
tbb <enesg@protonmail.com>
2018-01-25 19:44:16 +0300

    some fixes

--- js/script.js
+++ js/script.js
@@ -1,28 +1,36 @@
+var quoteText = "";
+var quoteAuthor = "";
+
 $(document).ready(function() {
   getNewQuote();
   $("#next").click(getNewQuote);
-});
-
-function getNewQuote() {
-  $.ajax({
-    url: "http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&jsonp=update&lang=en",
-    dataType: "jsonp"
-  });
-}
-
-function update(json) {
-  // update texts
-  $("#text p").text(json.quoteText);
-  $("#quoter").text(json.quoteAuthor);
-
-  // update share button
   $("#tweet").click(function() {
     var url = "https://twitter.com/intent/tweet?";
-    url += "text=" + encodeURIComponent("\"" + json.quoteText + "\" " + json.quoteAuthor);
+    url += "text=" + encodeURIComponent("\"" + quoteText + "\" " + quoteAuthor);
     url += "&url=" + encodeURIComponent("https://thebeastbelow.github.io/fcc-randomquotemachine/");
     url += "&hashtags=" + "randomquotemachine,fcc";
     url += "&via=" + "freecodecamp";
 
     window.location.href = url;
   });
+});
+
+function getNewQuote() {
+  $("#next i").addClass("fa-spin");
+  $.ajax({
+    url: "https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&jsonp=update&lang=en",
+    dataType: "jsonp"
+  });
+}
+
+function update(json) {
+  quoteText = json.quoteText;
+  quoteAuthor = json.quoteAuthor;
+
+  // update texts
+  $("#text p").text(quoteText);
+  $("#quoter").text(quoteAuthor);
+
+  // update next button
+  $("#next i").removeClass("fa-spin");
 }
